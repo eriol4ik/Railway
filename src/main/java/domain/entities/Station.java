77b7716310eae8b9db8abae6f52a23187a541447
Vadym Kovalchuk <eriol4ik@gmail.com>
2017-05-03 18:11:26 +0300

    Added constructors & getters/setters in entities

--- src/main/java/domain/entities/Station.java
+++ src/main/java/domain/entities/Station.java
@@ -1,6 +1,51 @@
 package domain.entities;
 
+import java.sql.Statement;
+
 public class Station {
     private Long stationId;
     private String name;
+
+    public Station() {}
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public Long getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(Long stationId) {
+        this.stationId = stationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 17;
+        hash += Long.hashCode(stationId) * 31;
+        hash += name.hashCode() * 31;
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+
+        Station other = (Station) obj;
+        if (this.stationId == null || !stationId.equals(other.stationId)) return false;
+
+        return name != null && name.equals(other.name);
+    }
 }
--- src/main/java/domain/entities/Train.java
+++ src/main/java/domain/entities/Train.java
@@ -17,5 +17,57 @@ public class Train {
 
     private Map<UnitRoute, TrainUnitRouteInfo> fullRoute;
 
+    public Train() {}
 
+    public Train(String trainId,
+                 LocalTime departureTime,
+                 Period duration,
+                 Map<Integer, CarriageType> carriageMap,
+                 Map<UnitRoute, TrainUnitRouteInfo> fullRoute) {
+        this.trainId = trainId;
+        this.departureTime = departureTime;
+        this.duration = duration;
+        this.carriageMap = carriageMap;
+        this.fullRoute = fullRoute;
+    }
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Period getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Period duration) {
+        this.duration = duration;
+    }
+
+    public Map<Integer, CarriageType> getCarriageMap() {
+        return carriageMap;
+    }
+
+    public void setCarriageMap(Map<Integer, CarriageType> carriageMap) {
+        this.carriageMap = carriageMap;
+    }
+
+    public Map<UnitRoute, TrainUnitRouteInfo> getFullRoute() {
+        return fullRoute;
+    }
+
+    public void setFullRoute(Map<UnitRoute, TrainUnitRouteInfo> fullRoute) {
+        this.fullRoute = fullRoute;
+    }
 }
