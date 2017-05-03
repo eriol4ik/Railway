66ff2b216b937c8d5a4c36c9d10590b10f0d3bac
Vadym Kovalchuk <eriol4ik@gmail.com>
2017-05-03 18:10:50 +0300

    Added Utils for checking entities & to manage connection

---
+++ src/main/java/utils/ConnectionManager.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    public static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void rollback(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
